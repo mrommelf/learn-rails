eca3530460f297aff4de54fed503ec31009cc34d
Mark Rommelfanger <mrommelf@acm.org>
2015-10-24 16:20:16 -0500

    Fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  email enabled in production config.action_mailer.default_url_options = { :host = > Rails.application.secrets.domain_name } 
+  # email enabled in production 
+  config.action_mailer.default_url_options = { :host = > Rails.application.secrets.domain_name } 
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.perform_deliveries = true 
   config.action_mailer.raise_delivery_errors = false 
